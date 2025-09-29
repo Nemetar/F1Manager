af6c50fd18eeb5f5cc31a0fe336bce4b99557f31
Théo Nairi <theo.nairi@intermedical.com>
2025-09-29 18:33:28 +0200

    [+] use tauri

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import MainLayout from './components/layouts/MainLayout.vue';
+</script>
+
+<template>
+  <MainLayout>
+    <template #left>
+      <h1 class="text-2xl font-bold">Left Sidebar</h1>
+      <p>This is the left sidebar content.</p>
+    </template>
+
+    <template #center>
+      <h1 class="text-2xl font-bold">Main Content</h1>
+      <p>This is the main content area.</p>
+    </template>
+
+    <template #right>
+      <h1 class="text-2xl font-bold">Right Sidebar</h1>
+      <p>This is the right sidebar content.</p>
+    </template>
+  </MainLayout>
+</template>
---
+++ src/components/layouts/MainLayout.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="grid h-screen w-screen grid-cols-4 gap-4 p-4">
+    <div class="bg-white-200 rounded-box border border-white p-4">
+      <slot name="left"></slot>
+    </div>
+
+    <div class="bg-white-300 rounded-box col-span-2 border border-white p-4">
+      <slot name="center"></slot>
+    </div>
+
+    <div class="bg-white-200 rounded-box border border-white p-4">
+      <slot name="right"></slot>
+    </div>
+  </div>
+</template>
